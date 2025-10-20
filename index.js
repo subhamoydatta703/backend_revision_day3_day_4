56b1ba93b7fe831a006d0f65293b2ae395c0caf1
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-10-20 11:25:26 +0530

    Adding the path

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const app= express();
 
 const port = 8080;
+
+app.set("views", path.join(__dirname, "/views"))
 
 app.set("view engine", "ejs");
 
