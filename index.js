44e26650997c525d5f2222cec4047f7e652b5a3c
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-10-20 11:02:32 +0530

    EJS rendering

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const app= express();
 
 const port = 8080;
 
+app.set("view engine", "ejs");
+
 app.get('/',(req, res)=>{
-    res.send("<h1>Hello</h1>")
+    // res.send("<h1>Hello</h1>");
+    res.render("home.ejs")
 })
 
 app.listen(port, ()=>{
